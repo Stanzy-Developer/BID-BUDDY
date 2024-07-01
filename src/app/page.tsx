9d6989ee964dbf2d6948c42d686e28a6c8088ea5
Stanzy-Developer <nnoupingnouping@gmail.com>
2024-07-01 07:07:47 +0100

    build improvement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,24 @@
 export default function HomePage() {
+  const mockUrls = [
+    "https://fvslhnpchvxhijfmpobi.supabase.co/storage/v1/object/public/Images/Free%20Images/Inbox_Zoho_Mail.png",
+    "https://fvslhnpchvxhijfmpobi.supabase.co/storage/v1/object/public/Images/Free%20Images/Inbox_Zoho_Mail.png",
+    "https://fvslhnpchvxhijfmpobi.supabase.co/storage/v1/object/public/Images/Free%20Images/__1_.jpg",
+    "https://fvslhnpchvxhijfmpobi.supabase.co/storage/v1/object/public/Images/Free%20Images/__1_.jpg",
+  ];
+  const mockImages = mockUrls.map((url, idx) => ({
+    id: idx + 1,
+    url,
+  }));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map(({ id, url }) => (
+          <div className="p-4" key={id}>
+            <img src={url} className="h-48 w-48 "/>
+          </div>
+        ))}
+      </div>
       <h1>Hello world</h1>
     </main>
   );
